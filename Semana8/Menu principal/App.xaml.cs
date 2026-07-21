9a15ccef38596fad5bb788b2c84ddbf8da27d07e
YESICA GUEVARA <yguevrao24_1@unc.edu.pe>
2026-07-20 20:01:53 -0500

    semna8

---
+++ Semana8/Menu principal/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Menu_principal
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Semana8/MenuContextual/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MenuContextual
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem menu = sender as MenuItem;
+
+            switch (menu.Header.ToString())
+            {
+                case "Menu Item 1":
+                    MessageBox.Show("Seleccionaste el Menú 1");
+                    break;
+
+                case "Menu Item 2":
+                    MessageBox.Show("Seleccionaste el Menú 2");
+                    break;
+
+                case "Menu Item 3":
+                    MessageBox.Show("Seleccionaste el Menú 3");
+                    break;
+
+                case "Salir":
+                    Close();
+                    break;
+            }
+        }
+        }
+}
